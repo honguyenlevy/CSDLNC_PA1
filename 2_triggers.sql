e0a1ba7a01209b4234035d4d4284a5f3dab65ddd
Yen Nhi Le <leyennhi112@gmail.com>
2021-10-30 23:24:45 +0700

    delete CTHD => HoaDon.TongTien is not deleted :((

---
+++ 2_triggers.sql
@@ -0,0 +1,61 @@
+﻿use LAB01
+go
+-----------------------------------------------------------
+--Cau 2. Cai cac trigger
+--a. Thành tiền CTHD = (Số lượng * (Giá bán - Giá giảm))
+alter trigger trigger_ThanhTien	
+on CT_HoaDon
+for insert, update, delete as
+if update (SoLuong) or update (GiaBan) or update (GiaGiam)
+begin
+	update	
+		CT_HoaDon
+	set 
+		ThanhTien = cthd.SoLuong * (cthd.GiaBan - cthd.GiaGiam)
+	from 
+		inserted i, SanPham sp, CT_HoaDon cthd
+	where 
+		sp.MaSP = i.MaSP and
+		sp.MaSP = cthd.MaSP
+end
+go
+
+--b. Tổng tiền (mahd) = sum (ThanhTien) cthd(mahd)
+alter trigger trigger_TongTien		
+on CT_HoaDon 
+for insert, update, delete 
+as
+if update (SoLuong) or update (GiaBan) or update (GiaGiam) 
+begin
+	update HoaDon 
+	set TongTien = (select sum(ThanhTien)
+					from CT_HoaDon
+					where CT_HoaDon.MaHD = HoaDon.MaHD)					
+	where 
+		exists (select * from inserted i where i.MaHD = HoaDon.MaHD) or
+		exists (select * from deleted d	 where d.MaHD = HoaDon.MaHD)
+end
+go
+
+---------------------------------------------------
+-- Test for Trigger 
+insert into SanPham values (N'AAB000', N'Hershey kisses', 19, '', 150000)
+insert into	CT_HoaDon(MaHD, MaSP, SoLuong, GiaBan, GiaGiam) values (N'ID000000', N'AAB000', 23, 150000, 5000)
+
+insert into SanPham values (N'AAA001', N'Kinder chocolate', 18, '', 140000)
+insert into	CT_HoaDon(MaHD, MaSP, SoLuong, GiaBan, GiaGiam) values (N'ID000000', N'AAA001', 20, 140000, 5000)
+
+delete from CT_HoaDon where MaHD = N'ID000000' and MaSP = N'AAB000'
+delete from CT_HoaDon where MaHD = N'ID000000' and MaSP = N'AAA001'
+
+delete from SanPham where TenSP = N'Hershey kisses'
+delete from SanPham where TenSP = N'Kinder chocolate'
+
+select * from SanPham
+select * from CT_HoaDon
+select * from HoaDon
+
+
+
+
+
